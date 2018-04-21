cf549e48b69ec03a68d51c8f0fb8e382d8f0df36
Hippunk <arthur.ramolet@hotmail.fr>
2018-04-21 19:19:06 +0200

    merge swipe with new chara model

--- Assets/Scripts/DialogTest.cs
+++ Assets/Scripts/DialogTest.cs
@@ -11,6 +11,7 @@ public class DialogTest : MonoBehaviour {
     void Start() {
         var characters = this.characterHandler.LoadCharacters();
         foreach (var c in characters) {
+            Debug.Log(c.dialog.GetChoices()[0]);
             Debug.Log(c.characterName + " " + c.titre + " (" + c.emotion + ") : " + c.dialog.GetChoices()[0].dialogue);
         }
     }

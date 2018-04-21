f4b77ca13cbcf7775fc4e7124ab55339604766de
Antoine Qu <qu_antoine@hotmail.fr>
2018-04-21 21:47:25 +0200

    bug fixing on Fighting System

--- Assets/FightingSystem.cs
+++ Assets/FightingSystem.cs
@@ -1,19 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
-public static class FightingSystem {
+public class FightingSystem {
 
 	public RPS playerChoice;
 	private RPS AIChoice;
 
 	private RPS[] randomChoiceTab = { RPS.Rock, RPS.Paper, RPS.Scisors };
 
-	private Result[][] resultTab = {
-		{ Result.DRAW, Result.LOSE, Result.WIN },
-		{ Result.WIN, Result.DRAW, Result.LOSE },
-		{ Result.LOSE, Result.WIN, Result.DRAW }
-	};
+	private Result[,] resultTab;
 
 	public enum Result {
 		WIN,
@@ -28,7 +25,18 @@ public static class FightingSystem {
 		NotPlayed = 3
 	}
 
-
+	public FightingSystem(){
+		resultTab = new Result[3,3];
+		resultTab [(int)RPS.Rock, (int)RPS.Rock] = Result.DRAW;
+		resultTab [(int)RPS.Rock, (int)RPS.Paper] = Result.LOSE;
+		resultTab [(int)RPS.Rock, (int)RPS.Scisors] = Result.WIN;
+		resultTab [(int)RPS.Paper, (int)RPS.Rock] = Result.WIN;
+		resultTab [(int)RPS.Paper, (int)RPS.Paper] = Result.DRAW;
+		resultTab [(int)RPS.Paper, (int)RPS.Scisors] = Result.LOSE;
+		resultTab [(int)RPS.Scisors, (int)RPS.Rock] = Result.LOSE;
+		resultTab [(int)RPS.Scisors, (int)RPS.Paper] = Result.WIN;
+		resultTab [(int)RPS.Scisors, (int)RPS.Scisors] = Result.DRAW;
+	}
 
 	public void engageBattle(FightingSystem.RPS AIDecision){
 		AIChoice = AIDecision;
@@ -55,11 +63,40 @@ public static class FightingSystem {
 		if (AIChoice == RPS.NotPlayed)
 			AIChoice = randomChoiceTab[(int) Random.Range (0, 2)];
 
-		returned = resultTab [playerChoice] [AIChoice];
+		returned = resultTab [(int)playerChoice, (int)AIChoice];
 
 		playerChoice = RPS.NotPlayed;
 		AIChoice = RPS.NotPlayed;
 	
 		return returned;
 	}
+
+	public void Testing(){
+		Result testResult;
+
+		try{
+			playerChoice = RPS.Rock;
+			AIChoice = RPS.Scisors;
+			Assert.AreEqual (hasWon (), Result.WIN);
+
+			playerChoice = RPS.Rock;
+			AIChoice = RPS.Rock;
+			Assert.AreEqual (hasWon (), Result.DRAW);
+
+			playerChoice = RPS.Rock;
+			AIChoice = RPS.Paper;
+			Assert.AreEqual (hasWon (), Result.LOSE);
+
+			playerChoice = RPS.Paper;
+			AIChoice = RPS.Rock;
+			Assert.AreEqual (hasWon (), Result.WIN);
+
+		} catch (AssertionException e){
+			Debug.Log ("RPS Fail");
+		}
+
+		playerChoice = RPS.NotPlayed;
+		AIChoice = RPS.NotPlayed;
+	}
+
 }

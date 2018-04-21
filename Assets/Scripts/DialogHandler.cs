b0ab8fbd7d82fd9ca8443a6989d4307461ef6f6c
Hippunk <arthur.ramolet@hotmail.fr>
2018-04-21 17:01:43 +0200

    recherche sur scripts dialogues

---
+++ Assets/Scripts/DialogHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogHandler : MonoBehaviour {
+    
+    public string pathToDialog;
+
+	// Use this for initialization
+	void Start () {
+        //DialogueFile dialogFile = ;
+
+        DialogueManager manager = DialogueManager.LoadDialogueFile(pathToDialog);
+
+        Dialogue currentDialogue = manager.GetDialogue("newDialogue");
+
+        Dialogue.Choice currentChoice = currentDialogue.GetChoices()[0];
+
+        currentDialogue.PickChoice(currentChoice);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

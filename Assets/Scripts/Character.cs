106fd2731b2d153a58fd4ae584b8cc1f515a96d2
Vincent <vincent@kilosaurus.com>
2018-04-21 18:15:05 +0200

    Character Update

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character {
+
+    public string characterName;
+    public string titre;
+    public int relation;
+    public float emotion;
+
+}
---
+++ Assets/Scripts/CharacterData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "CreateCharacter")]
+public class CharacterData : ScriptableObject {
+
+    [Header("Character Name")]
+    public string characterName;
+
+    [Header("Titre")]
+    public string titre;
+
+    [Header("Relation")]
+    public int relation;
+
+    [Header("Emotion")]
+    public Emotion emotion;
+
+}
+
+public enum Emotion { joy, sadness, anger, disgust, embarrassment }

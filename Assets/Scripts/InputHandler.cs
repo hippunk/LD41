7cc04b021f0db0ac4059915e4a525691b61905d2
Vincent <vincent@kilosaurus.com>
2018-04-23 14:10:43 +0200

    PlaceHolder fix

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -7,6 +7,10 @@ public class InputHandler : MonoBehaviour {
 
     public InputField input;
 
+    private void Start() {
+        this.input.placeholder.GetComponent<Text>().text = GameConstant.PLAYER_NAME;
+    }
+
     public void OnPseudoEdit() {
         GameLogicManager.playerName = this.input.text;
         Debug.Log(GameLogicManager.playerName);
